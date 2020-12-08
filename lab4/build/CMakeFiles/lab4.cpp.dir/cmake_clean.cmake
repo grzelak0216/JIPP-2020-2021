c93fdb2e6fd0460f96dc6071986cd42993522086
Grzegorz Lak <72693936+grzelak0216@users.noreply.github.com>
2020-12-08 10:39:55 +0100

    dodanie lab4

---
+++ lab4/build/CMakeFiles/lab4.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4.cpp.dir/main.cpp.obj"
+  "lab4.cpp.exe"
+  "lab4.cpp.exe.manifest"
+  "lab4.cpp.pdb"
+  "liblab4.cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
