21369f8750045283fd0a74b4959b6d46131db719
Grzegorz Lak <72693936+grzelak0216@users.noreply.github.com>
2020-12-29 22:01:31 +0100

    add new laboratories

---
+++ lab5/include/header.hpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Macierz
+{
+protected:
+private:
+    double **macierz;
+    int kol;
+    int wier;
+
+public:
+    //Konstruktory
+    Macierz(int ilkol, int ilwier) //Konstruktor macierzy prostokatnej
+    {
+        kol = ilkol;
+        wier = ilwier;
+        macierz = new double *[ilkol];
+        for (int j = 0; j < ilwier; j++)
+        {
+            macierz[j] = new double[ilkol];
+
+            for (int i = 0; i < ilkol; i++)
+            {
+                macierz[j][i] = 0;
+            }
+        }
+    }
+
+    // Macierz(string filename, string path) //Konstruktor macierzy pobranej z pliku
+    // {
+    //     fstream plik;
+    //     plik.open(path + filename, ios::in);
+    //     if (plik.good() == false)
+    //     {
+    //         cout << "\n Plik ERROR \n";
+    //         exit(0);
+    //     }
+    //     else
+    //     {
+    //         plik >> Macierz::kol;
+    //         cout << kol << " ";
+    //         plik >> Macierz::wier;
+    //         cout << wier << " \n";
+
+    //         macierz = new double *[kol];
+    //         for (int j = 0; j < kol; j++)
+    //         {
+    //             macierz[j] = new double[wier];
+
+    //             for (int i = 0; i < wier; i++)
+    //             {
+    //                 plik >> Macierz::macierz[j][i];
+    //                 cout << " " << macierz[j][i] << " ";
+    //             }
+    //             cout << endl;
+    //         }
+    //     }
+    //     plik.close();
+    // }
+
+    void destroy() //Destruktor
+    {
+        for (int j = 0; j < wier; j++)
+        {
+            delete[] macierz[j];
+        }
+        delete[] macierz;
+        cout << " destruktor \n";
+    }
+
+    void set(int x, int y, double wart) //Ustawianie wartosci w macierzy
+    {
+        macierz[x][y] = wart;
+    }
+
+    int raws()
+    {
+        return kol;
+    }
+
+    void print() //Wyswietlanie macierzy
+    {
+        for (int j = 0; j < wier; j++)
+        {
+            for (int i = 0; i < kol; i++)
+            {
+                cout << "[ " << macierz[j][i] << " ] ";
+            }
+            cout << "\n";
+        }
+    }
+    /*////////////////////////////////////////*/
+    Macierz operator+(const Macierz Macierz2) const
+    {
+        Macierz Wynik(Macierz2.kol, Macierz2.wier);
+
+        for (int j = 0; j < wier; j++)
+        {
+            for (int i = 0; i < kol; i++)
+            {
+                Wynik.set(j, i, (macierz[j][i] + Macierz2.macierz[j][i]));
+            }
+            cout << "\n";
+        }
+        return Wynik;
+    }
+
+    Macierz operator-(const Macierz Macierz2) const
+    {
+        Macierz Wynik(Macierz2.kol, Macierz2.wier);
+
+        for (int j = 0; j < wier; j++)
+        {
+            for (int i = 0; i < kol; i++)
+            {
+                Wynik.set(j, i, (macierz[j][i] - Macierz2.macierz[j][i]));
+            }
+            cout << "\n";
+        }
+        return Wynik;
+    }
+
+    Macierz operator*(const Macierz Macierz2) const
+    {
+        Macierz Wynik(Macierz2.kol, wier);
+
+        double wart;
+        for (int j = 0; j < wier; j++)
+        {
+            for (int i = 0; i < Macierz2.kol; i++)
+            {
+                wart = 0;
+                for (int o = 0; o < kol; o++)
+                {
+                    wart += macierz[j][o] * Macierz2.macierz[o][i];
+                }
+                //cout << "\n " << wart;
+                Wynik.set(j, i, wart);
+            }
+        }
+        return Wynik;
+    }
+
+    friend void operator<<(std::ofstream &file, const Macierz &Macierz_save)
+    {
+        int i, j;
+        file << Macierz_save.kol << " " << Macierz_save.wier << endl;
+
+        for (j = 0; j < Macierz_save.wier; j++)
+        {
+            for (i = 0; i < Macierz_save.kol; i++)
+            {
+                file << Macierz_save.macierz[j][i] << " ";
+            }
+
+            file << endl;
+        }
+    }
+
+    void operator==(Macierz Macierz2) const
+    {
+        int val = 1;
+        if (kol == Macierz2.kol && wier == Macierz2.wier)
+        {
+            for (int j = 0; j < wier; j++)
+            {
+                for (int i = 0; i < kol; i++)
+                {
+                    if (macierz[j][i] != Macierz2.macierz[j][i])
+                    {
+                        val = 0;
+                    }
+                }
+            }
+        }
+        else
+        {
+            val = 0;
+        }
+
+        if (val == 1)
+        {
+            cout << "\n Macierze sa takie same \n";
+        }
+        else
+        {
+            cout << "\n Macierze nie sa takie same \n";
+        }
+    }
+
+    double operator[](int n)
+    {
+
+        if ((n < 0) || (n > wier))
+        {
+            if (n < 0)
+            {
+                n = 0;
+            }
+            else
+            {
+                n = wier;
+            }
+        }
+        return *macierz[n];
+    }
+};
---
+++ lab5/src/main.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <time.h>
+#include "../include/header.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // srand(time(NULL));
+    int k, w;
+    double pom;
+
+    k = 3;
+    w = 2;
+
+    Macierz Macierz1(k, w);
+
+    for (int j = 0; j < w; j++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            pom = rand() % 10;
+            //pom = i + j;
+            //cout << pom << " ";
+            Macierz1.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    Macierz Macierz2(k, w);
+
+    for (int j = 0; j < w; j++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            pom = rand() % 10;
+            //pom = i + j;
+            //cout << pom << " ";
+            Macierz2.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    cout << "\n macierz1: \n";
+    Macierz1.print(); //Wyswietlanie zawatrosci macierzy
+    cout << "\n macierz2: \n";
+    Macierz2.print(); //Wyswietlanie zawatrosci macierzy
+
+    cout << "\n poruwnywanie\n"
+         << endl;
+    Macierz1 == Macierz2;
+
+    Macierz Macierz3(k, w);
+
+    Macierz3.print(); //Wyswietlanie zawatrosci macierzy
+    cout << endl;
+
+    cout << "\n dodawanie\n"
+         << endl;
+
+    Macierz3 = Macierz1 + Macierz2;
+
+    cout << "\n wynik :\n";
+    Macierz3.print(); //Wyswietlanie zawatrosci macierzy
+    cout << endl;
+
+    for (int j = 0; j < w; j++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            pom = rand() % 10;
+            //pom = i + j;
+            //cout << pom << " ";
+            Macierz1.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    for (int j = 0; j < w; j++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            pom = rand() % 10;
+            //pom = i + j;
+            //cout << pom << " ";
+            Macierz2.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    cout << "\n odejmowanie \n macierz1: \n";
+    Macierz1.print();
+    cout << "\n macierz1: \n";
+    Macierz2.print();
+
+    Macierz1 = (Macierz3 - Macierz2);
+
+    cout << "\n wynik :\n";
+    Macierz1.print(); //Wyswietlanie zawatrosci macierzy
+    cout << endl;
+
+    cout << "\n poruwnywanie\n"
+         << endl;
+    Macierz1 == Macierz2;
+
+    Macierz Multiply1(k, k);
+
+    for (int j = 0; j < k; j++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            pom = rand() % 10;
+            //pom = i + j;
+            //cout << pom << " ";
+            Multiply1.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    Macierz Multiply2(k, k);
+
+    for (int j = 0; j < k; j++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            pom = rand() % 10;
+            //pom = i + j;
+            //cout << pom << " ";
+            Multiply2.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    cout << "\n mnorzenie \n macierz1: \n";
+    Multiply1.print(); //Wyswietlanie zawatrosci macierzy
+    cout << "\n macierz2: \n";
+    Multiply2.print(); //Wyswietlanie zawatrosci macierzy
+
+    Macierz3 = Multiply1 * Multiply2;
+
+    cout << "\n wynik :\n";
+    Macierz3.print();
+    cout << "\n";
+
+    ofstream file;
+    file.open("matrix.txt");
+    file << Macierz3;
+    file.close();
+
+    Macierz Macierz4(4, 4);
+
+    for (int j = 0; j < 4; j++)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            pom = i + j;
+            Macierz4.set(j, i, pom);
+        }
+        //cout << "\n";
+    }
+
+    static int n = Macierz4.raws();
+
+    double tab[n] = {Macierz4[0]};
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    Macierz4.destroy();
+    Macierz3.destroy();
+    Multiply1.destroy();
+    Multiply2.destroy();
+    Macierz2.destroy();
+    Macierz1.destroy();
+
+    //koniec
+    return 0;
+}
